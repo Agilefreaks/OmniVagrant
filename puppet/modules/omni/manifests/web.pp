ba88cc71e12f672b4303658569a20d76ab474dd4
Calin <calinoiu.alexandru@agilefreaks.com>
2014-06-02 13:56:09 +0300

    web does not update to latest master

--- puppet/modules/omni/manifests/web.pp
+++ puppet/modules/omni/manifests/web.pp
@@ -3,14 +3,14 @@ class omni::web (
     $ruby_version = 'ruby-2.1.2',
     $gemset = 'webomni'
   ) {
-  vcsrepo { "/home/$user/omni/webomni":
-    ensure => latest,
-    provider => git,
-    require => [ Package[ 'git' ] ],
-    source => 'git@github.com:Agilefreaks/WebOmni.git',
-    revision => 'master',
-    identity => "/home/$user/.ssh/id_rsa"
-  }->
+  # vcsrepo { "/home/$user/omni/webomni":
+  #   ensure => latest,
+  #   provider => git,
+  #   require => [ Package[ 'git' ] ],
+  #   source => 'git@github.com:Agilefreaks/WebOmni.git',
+  #   revision => 'master',
+  #   identity => "/home/$user/.ssh/id_rsa"
+  # }->
   exec { 'web: install gems':
     command => "cd omni/webomni && /bin/bash --login -c 'rvm use $ruby_version@$gemset do bundle install'",
     provider => shell,
